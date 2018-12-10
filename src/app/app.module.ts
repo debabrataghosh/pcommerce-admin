a383135f4efc8b93813f95ee8eda178cfc9ed2a5
Debabratagwl <debabrata.ghosh@goodworklabs.com>
2018-12-10 18:42:48 +0530

    admin page added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoryComponent } from './category/category.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule
