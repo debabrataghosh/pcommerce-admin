92279c9c0c7c3a4f3651b936bcc5b9e9658e2206
Debabratagwl <debabrata.ghosh@goodworklabs.com>
2018-12-11 16:55:46 +0530

    product, view and add edit component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,41 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CategoryComponent } from './category/category.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { RouterModule } from '@angular/router';
+import { ViewCategoryComponent } from './view-category/view-category.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { AddEditCategoryComponent } from './add-edit-category/add-edit-category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CategoryComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    ViewCategoryComponent,
+    ViewProductComponent,
+    AddEditCategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path : '',
+        component : CategoryComponent
+      },
+      {
+        path : 'category/addEdit',
+        component : AddEditCategoryComponent
+      },
+      {
+      path: 'category/view',
+      component : ViewCategoryComponent
+      },
+      {
+        path : 'category/product',
+        component : ViewProductComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
